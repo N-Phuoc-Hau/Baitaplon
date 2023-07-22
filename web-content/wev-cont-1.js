19fef7e675f1ab5a6331966ea8346de0f93d1c5e
phuoc hau <binpe09x@gmail.com>
2023-07-22 22:49:53 +0700

    aa
    aa

--- web-content/wev-cont-1.js
+++ web-content/wev-cont-1.js
@@ -1,5 +1,12 @@
-$(document).ready(function(){
-   if ($('.menu').srcollTop(800)){
-        $(this).attr('position','fixed');
+// $(document).ready(function(){
+//    if ($('.menu').srcollTop(800)){
+//         $(this).attr('position','fixed');
+//     }
+// });
+window.onscroll = function(){
+    console.info(document.body.scrollTop)
+    if(document.documentElement.scrollTop > 490){
+        var header = document.getElementById("menu");
+        header.style.position = "fixed";
     }
-});
+}
