e7038cd84a2e984d408f5db155eac5e86aa511ae
minhkiet <kiet7784@gmail.com>
2023-07-26 21:17:42 +0700

    add

--- link-log-in/log-in.js
+++ link-log-in/log-in.js
@@ -8,3 +8,28 @@ $(document).ready(function(){
         else  $(this).prev().attr('type','password');
     });
 });
+
+// validation form login
+const inputUsername = document.querySelector("#username");
+const inputPassword = document.querySelector("#password");
+const btnLogin = document.querySelector(".submit");
+
+// validation form login
+
+btnLogin.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (inputUsername.value === "" || inputPassword.value === "") {
+    alert("vui lòng không để trống");
+  } else {
+    const user = JSON.parse(localStorage.getItem(inputUsername.value));
+    if (
+      user.username === inputUsername.value &&
+      user.password === inputPassword.value
+    ) {
+      alert("Đăng Nhập Thành Công");
+      window.location.href = "../BTL.html";
+    } else {
+      alert("Đăng Nhập Thất Bại");
+    }
+  }
+});
