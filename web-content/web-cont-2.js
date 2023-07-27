fcb6790066b258638e3f6c7a49c8fc8a4e5f3966
phuoc hau <binpe09x@gmail.com>
2023-07-27 15:05:41 +0700

    slider

---
+++ web-content/web-cont-2.js
@@ -0,0 +1,36 @@
+// $(document).ready(function(){
+//    if ($('.menu').srcollTop(800)){
+//         $(this).attr('position','fixed');
+//     }
+// });
+window.onscroll = function(){
+    console.info(document.body.scrollTop)
+    if(document.documentElement.scrollTop > 490){
+        var header = document.getElementById("menu");
+        header.style.position = "fixed";
+    }
+    else{
+        var header = document.getElementById("menu");
+        header.style.position = "relative";
+    }
+}
+//JSGoToTop
+var gototop = document.getElementById("gTTop");
+window.onscroll = function(){
+    console.info(document.body.scrollTop);
+    var gototop = document.getElementById("gTTop");
+    if(document.documentElement.scrollTop > 300){
+        gototop.style.display = "block";
+    }
+    else{
+        gototop.style.display = "none";
+    }
+}
+gototop.addEventListener('click', e=>{
+    var timer = setInterval(function(){
+        document.documentElement.scrollTop-=20;
+
+        if (document.documentElement.scrollTop <= 0)
+        clearInterval(timer);
+    }, 5)
+})
