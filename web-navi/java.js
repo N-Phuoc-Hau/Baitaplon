cf616192b5eb6d87292c1cc66b725da9d69d0f99
phuoc hau <binpe09x@gmail.com>
2023-08-13 18:31:38 +0700

    navi

--- web-navi/java.js
+++ web-navi/java.js
@@ -105,10 +105,20 @@ function loadforumsnor(){
         d.innerHTML += re;
     })
 }
+function loadmenu(){
+    fetch("nav.json").then(res => res.json()).then(data =>{
+        let d = document.getElementById("nav");
+        let re = "";
+        for (let c of data)
+        re += `<li><span title="${c.title}"><a href="${c.href}"><i class="${c.icon}"></i></a></span></li>`;
 
+        d.innerHTML += re;
+    })
+}
 
 window.onload = () => {
     loadstatus();
     loadforumsnor();
     loadforumspro();
+    loadmenu();
 }
